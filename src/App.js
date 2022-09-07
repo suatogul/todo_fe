af6d41122fae0eb886ba769473bb61c927728c8d
Suat OĞUL <suatogul@gmail.com>
2022-09-07 20:59:10 +0200

    todos listed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Container, Row } from "react-bootstrap"
-import { RiDeleteBin6Fill } from "react-icons/ri"
+import PrintTodos from './Component/PrintTodos';
 function App() {
   return (
     <div className="App">
@@ -9,13 +9,13 @@ function App() {
           <div className='col-8 my-5 ms-5 ps-5'>
             <div className='main-container col-10 my-3 ms-5 ps-5'>
               <h3>Todo List</h3>
-              <form class="row g-2">
-                <div class="col-5">
-                  <label for="todo-text" class="visually-hidden"></label>
-                  <input type="text" class="form-control" id="todo-text" placeholder="Add New Todo" />
+              <form className="row g-2">
+                <div className="col-5">
+                  <label htmlFor="todo-text" className="visually-hidden"></label>
+                  <input type="text" className="form-control" id="todo-text" placeholder="Add New Todo" />
                 </div>
-                <div class="col-auto">
-                  <button type="submit" class="btn btn-primary mb-3">Add</button>
+                <div className="col-auto">
+                  <button type="submit" className="btn btn-primary mb-3">Add</button>
                 </div>
               </form>
               {/* <div className='input-todo col-5'>
@@ -23,31 +23,9 @@ function App() {
                 <button class="btn btn-primary btn-sm mb-1 ms-2" type="submit">Add</button>
               </div> */}
               <div className='todo-list col-6'>
-                <div class="list-group">
+                <div className="list-group">
 
-                  <div className='d-flex mb-3'>
-                    <div className='flex-grow-1'>
-                      <label class="list-group-item">
-                        <input class="form-check-input me-1" type="checkbox" value="" />
-                        First checkbox
-                      </label>
-                    </div>
-                    <div className='bg-danger d-flex align-items-center'>
-                      <span className='px-3 py-0 m-0 fs-5'><RiDeleteBin6Fill /></span>
-                    </div>
-                  </div>
-
-                  <div className='d-flex'>
-                    <div className='flex-grow-1'>
-                      <label class="list-group-item">
-                        <input class="form-check-input me-1" type="checkbox" value="" />
-                        Second checkbox
-                      </label>
-                    </div>
-                    <div className='bg-danger d-flex align-items-center'>
-                      <span className='px-3 py-0 m-0 fs-5'><RiDeleteBin6Fill /></span>
-                    </div>
-                  </div>
+                  <PrintTodos />
 
                 </div>
               </div>
